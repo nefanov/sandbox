44b37a313b125043a31281cd0b65281d8c79b112
nefanov <efanov@phystech.edu>
2022-04-27 18:45:42 +0300

    Update grammar_editor.py

--- pathfinder/grammar_editor.py
+++ pathfinder/grammar_editor.py
@@ -1,5 +1,11 @@
 TOKEN_LIST = ["any any","any assign_const"]
 
+index = 0;
+def inc_unique_suffix():
+  global index
+  index += 1
+  return str(index)
+
 class Term: # for terminal and non-terminal symbols
   def __init__(self, N="A"):
     self.name = N
@@ -19,7 +25,7 @@ class Grammar:
     self.P = list()
     pass
   
-  def add_rule(self, L, R)
+  def add_rule(self, L, R):
     left_side = L
     for r in R:
       if r.releatable:
@@ -33,3 +39,10 @@ class Grammar:
   def add_alternative(self, rule, right):
     add_rule(rule[0], right)
     return
+
+def test1():
+  print(inc_unique_suffix(),inc_unique_suffix(), index)
+  
+  
+if __name__=='__main__':
+  test1()
