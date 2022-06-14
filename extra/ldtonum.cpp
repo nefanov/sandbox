e5868e1e65f3411e99456b16d3a3aa74fafbae6d
nefanov <efanov@phystech.edu>
2022-06-14 14:58:15 +0300

    Update ldtonum.cpp

--- extra/ldtonum.cpp
+++ extra/ldtonum.cpp
@@ -1,10 +1,8 @@
 /******************************************************************************
-
 Welcome to GDB Online.
 GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
 C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
-
 *******************************************************************************/
 #include <iostream>
 #include <cstdio>
@@ -49,8 +47,16 @@ void cvt(long double ldd) {
     return;
 }
 
+
+
+#define ALPHA   0x1.405f49063806f6e0ca86fd58fe82p1
+
+#define EXPAND_SUFFIX_L(what) what##L
+#define SUFFIX_L(       what) EXPAND_SUFFIX_L(what)
+
 int main()
 {
-    cvt(2.0L);
+    cvt(SUFFIX_L(ALPHA));
+    std::cout<< sizeof(SUFFIX_L(ALPHA)) <<std::endl;
     return 0;
 }
