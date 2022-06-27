ca3379b0d74a64350776696350eae4bf816b41e5
nefanov <efanov@phystech.edu>
2022-06-27 12:32:24 +0300

    Update test_arr.c

--- extra/test_arr.c
+++ extra/test_arr.c
@@ -2,8 +2,8 @@
 
 int main() {
         long double arr[2];
-        arr[0]=__builtin_nan("");
-        arr[1]=__builtin_inf();
+        arr[0]=__builtin_nanl("");
+        arr[1]=__builtin_infl();
         int i;
         for (i=0;i<2;i++) {
                 printf("Number :\n");
